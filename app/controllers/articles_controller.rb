cbfb6ce371eb1bb8e410ddc6366948761cc2e233
Jodi Collins <jodi@cosky.com>
2022-06-13 15:37:28 -0700

    Comments on controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,10 @@
 class ArticlesController < ApplicationController
-
+  # used to display a single Article item
   def show
     @article = Article.find(params[:id])
   end
 
+  # used to display a summarized listing of al items in the Article table
   def index
       @articles = Article.all
   end
