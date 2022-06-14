6e40b12617ebf4354dffbe479f1523334ad79e2a
Jodi Collins <jodi@cosky.com>
2022-06-13 17:28:12 -0700

    Add article routes for new and create, create form for new articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,4 +9,21 @@ class ArticlesController < ApplicationController
       @articles = Article.all
   end
 
+  def new
+
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    #render plain: params[:article]
+    #render plain: @article.inspect
+    
+    #save to database
+    @article.save
+
+    #redirect to show page
+    #redirect_to article_path(@article)
+    #shortcut for redirect 
+    redirect_to @article
+  end
 end
